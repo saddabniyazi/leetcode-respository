ff4102116786cb8a8b35ed07fd6c9f83596e7a18
md. saddab niyazi <143011440+saddabniyazi@users.noreply.github.com>
2026-06-27 02:42:07 +0530

    Time: 39 ms (26.72%), Space: 80.2 MB (92.49%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        
+        int n=costs.size();
+        sort(costs.begin(),costs.end());
+        int ans=0;
+        int i=0;
+        while(i<n && coins>=costs[i]){
+            ans++;
+            coins=coins-costs[i];
+            i++;
+        }
+        return ans;
     }
 };
