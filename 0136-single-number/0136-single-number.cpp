d2d8cb8d2830daa1ab7b975c181c296790c20ea9
md. saddab niyazi <143011440+saddabniyazi@users.noreply.github.com>
2026-07-08 13:20:47 +0530

    Time: 0 ms (100%), Space: 20.7 MB (58.7%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        int x=nums[0];
+        int ans=0;
+        for(int i=1;i<n;i++){
+            x=x^nums[i];
+        }
+        return x;
+    }
+};
