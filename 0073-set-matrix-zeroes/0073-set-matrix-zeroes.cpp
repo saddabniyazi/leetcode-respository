ca6be4ec7a5a56977ca2463917fd790c976c4d4f
md. saddab niyazi <143011440+saddabniyazi@users.noreply.github.com>
2026-07-11 06:37:01 +0530

    Time: 3 ms (29.41%), Space: 20.8 MB (76.94%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,28 +3,22 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
+        vector<int>row(n,0);
+        vector<int>col(m,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                for(int row=0;row<n;row++){
-                    if(matrix[row][j]!=0){
-                        matrix[row][j]=-10;
-                    }
-                }
-                for(int col=0;col<m;col++){
-                    if(matrix[i][col]!=0){
-                        matrix[i][col]=-10;
-                    }
-                }
+                    row[i]=-10;
+                    col[j]=-10;
                 }
             }
         }
-            for(int i=0;i<n;i++){
-                for(int j=0;j<m;j++){
-                    if(matrix[i][j]==-10){
-                        matrix[i][j]=0;
-                    }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row[i]==-10 || col[j]==-10){
+                    matrix[i][j]=0;
                 }
             }
+        }
     }
 };
