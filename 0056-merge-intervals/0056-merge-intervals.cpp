74ee5ed17c8dd6d9f205df724ed237dd2dcf46c0
md. saddab niyazi <143011440+saddabniyazi@users.noreply.github.com>
2026-07-14 09:13:21 +0530

    Time: 8 ms (23.91%), Space: 23.7 MB (82.43%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        for(int i=0;i<n;i++){
+            if(!ans.empty() && intervals[i][1]<=ans.back()[1]) continue;
+              int  start=intervals[i][0];
+               int end=intervals[i][1];
+            for(int j=i+1;j<n;j++){
+                if(intervals[j][0]<=end){
+                    end=max(end,intervals[j][1]);
+                }
+                else{
+                    break;
+                }
+            }
+            ans.push_back({start,end});
+            }
+            return ans;
+    }
+};
