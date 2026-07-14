ce8e519cd920137b8beef862a91f5c0dc4f132d2
md. saddab niyazi <143011440+saddabniyazi@users.noreply.github.com>
2026-07-14 18:13:49 +0530

    Time: 3 ms (88.92%), Space: 23.9 MB (30.7%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,19 +5,19 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>>ans;
         for(int i=0;i<n;i++){
-            if(!ans.empty() && intervals[i][1]<=ans.back()[1]) continue;
-              int  start=intervals[i][0];
-               int end=intervals[i][1];
+            if(!ans.empty() && intervals[i][0]<=ans.back()[1]) continue;
+            int start=intervals[i][0];
+            int end=intervals[i][1];
             for(int j=i+1;j<n;j++){
-                if(intervals[j][0]<=end){
-                    end=max(end,intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+              if(intervals[j][0]<=end){
+                 end=max(end,intervals[j][1]);
+              }
+              else{
+                break;
+              }
             }
             ans.push_back({start,end});
-            }
-            return ans;
+        }
+        return ans;
     }
 };
