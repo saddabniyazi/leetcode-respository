61a3ceabe861977b3f6ac5a60faee41b1ddfe4d3
md. saddab niyazi <143011440+saddabniyazi@users.noreply.github.com>
2026-07-26 02:30:50 +0530

    Time: 36 ms (66.18%), Space: 66.9 MB (83.38%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int,int>>pq;
+        int n=points.size();
+        for(int i=0;i<n;i++){
+            int x=points[i][0];
+            int y=points[i][1];
+            int res=x*x+y*y;
+            pq.push({res,i});
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+          vector<vector<int>>ans;
+          while(pq.empty()==false){
+              int q=points[pq.top().second][0];
+              int r=points[pq.top().second][1];
+              ans.push_back({q,r});
+              pq.pop();
+          }
+          return ans;
+    }
+};
