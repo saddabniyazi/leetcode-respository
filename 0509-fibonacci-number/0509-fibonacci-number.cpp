5790b582936fcb7a5df9414ac43ee26fdc97bf2e
md. saddab niyazi <143011440+saddabniyazi@users.noreply.github.com>
2026-08-05 04:02:09 +0530

    Time: 0 ms (100%), Space: 7.9 MB (38.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int>dp(n+1,0);
-        if(n==0 || n==1) return n; 
-        dp[0]=0;
-        dp[1]=1;
+        if(n==0 || n==1) return n;
+        int prev1=1;
+        int prev2=0;
+        int next=0;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+           next=prev1+prev2;
+           prev2=prev1;
+           prev1=next;
         }
-        return dp[n];
+        return prev1;
     }
 };
