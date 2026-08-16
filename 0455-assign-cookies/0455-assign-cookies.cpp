2c8a41c3ea99bab873f4f6c29541952cf154a94b
md. saddab niyazi <143011440+saddabniyazi@users.noreply.github.com>
2026-08-17 03:38:38 +0530

    Time: 6 ms (75.76%), Space: 44.9 MB (48.28%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n=g.size();
+        int m=s.size();
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int l=0,r=0;
+        int cnt=0;
+        while(l<n && r<m){
+          if(s[r]>=g[l]){
+             cnt++;
+             l++;
+          }
+              r++;
+        }
+        return cnt;
+    }
+};
